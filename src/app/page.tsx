b68654271fb9b5e549c695266105089e4bc249f7
kaustubhraut-07 <kaustubh.raut@fxis.ai>
2024-10-08 18:23:59 +0530

    default route change to login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
-
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/login');
+  }, [router]);
+
   return (
     <div>
       Home
